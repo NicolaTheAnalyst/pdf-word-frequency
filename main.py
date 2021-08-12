fbdf20cdc75f33f072a8f872003ea352bbb38892
flyingwonton <happyviolett@gmail.com>
2021-08-12 10:25:12 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import PyPDF2
+import re
+import sys
+import collections
+import nltk
+from nltk.corpus import stopwords
+nltk.download("stopwords")
+stop_words = set(stopwords.words("italian"))
+
+def main():
+    #read the file and get the data
+    try:
+        path = input("Please provide the absolute path for the PDF file: ") 
+        pdf_file = open(path, mode="rb")
+
+    except FileNotFoundError:
+        print("File not found. Please check the inserted path.")
+        main()
+
+    except:
+        print("An error occurred ", sys.exc_info()[0])
+        exit()
+
+    read_pdf = PyPDF2.PdfFileReader(pdf_file)
+    pdfpages = read_pdf.getNumPages() 
+    i = 0
+    text = ""
+    for i in range(pdfpages):
+        page = read_pdf.getPage(i)
+        page_content = page.extractText()
+        text += " " + page_content
+        i += 1
+    datahandling(text)
+    newtext = datahandling(text)
+    dataexport(newtext)
+ 
+def datahandling(text):
+    #cleaning and tidying up
+    text = " ".join([word for word in text.split() if word not in stop_words]) # removes stop words by adding only word not in the stop word list
+    text = re.sub("https?:\/\/.*[\r\n]*", "", text) #remove urls
+    text = re.sub("[\(\)\\\/\;\,\.\[\]\{\}\-\_\!\?\=\:\'\"]*", "", text) #removing punctuaction, parenthesis and stuff
+    textlist = text.replace("\n", "").lower().split(" ") #removes newline from string (\n) -> makes it all lowercase -> splits the string in a list
+    #lower_case = [word.lower() for word in textlist]
+    counts_text = collections.Counter(textlist)
+    return counts_text
+
+def dataexport(data):
+    #exporting the data
+    try:
+        writer = pd.ExcelWriter('export.xlsx', engine='xlsxwriter')
+        df = pd.DataFrame((list(data.items())), columns=['words', 'count'])
+        df.to_excel(writer, index=False)
+        writer.save()
+        print("Export salvato.")
+    except:
+        print("An error occurred ", sys.exc_info()[0])
+        exit()
+
+if __name__ == '__main__':
+    main()
