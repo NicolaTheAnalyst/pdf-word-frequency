ac5e9195738ab6a29597e7cd95609a6f99106836
flyingwonton <happyviolett@gmail.com>
2021-08-13 10:34:02 +0200

    added filename feature

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import PyPDF2
 import re
 import sys
 import collections
+import os
 import nltk
 from nltk.corpus import stopwords
 nltk.download("stopwords")
@@ -15,6 +16,8 @@ def main():
         pdf_file = open(path, mode="rb")
         p = input("Please provide from which PDF page start counting: ")
 
+        #print(filename)
+
     except FileNotFoundError:
         print("File not found. Please check the inserted path.")
         main()
@@ -22,7 +25,8 @@ def main():
     except:
         print("An error occurred ", sys.exc_info()[0])
         exit()
-
+    head, tail = os.path.split(path)
+    filename = tail.split(".")[0] #splits the tail (eg. file.pdf) and get the name without the extension
     read_pdf = PyPDF2.PdfFileReader(pdf_file)
     pdfpages = read_pdf.getNumPages() 
     i = int(p) - 1
@@ -36,22 +40,22 @@ def main():
         i += 1
     datahandling(text)
     newtext = datahandling(text)
-    dataexport(newtext)
+    dataexport(newtext, filename)
  
 def datahandling(text):
     #cleaning and tidying up
     text = " ".join([word for word in text.split() if word not in stop_words]) # removes stop words by adding only word not in the stop word list
     text = re.sub("https?:\/\/.*[\r\n]*", "", text) #remove urls
     text = re.sub("[\(\)\\\/\;\,\.\[\]\{\}\-\_\!\?\=\:\'\"]*", "", text) #removing punctuaction, parenthesis and stuff
-    textlist = text.replace("\n", "").lower().split(" ") #removes newline from string (\n) -> makes it all lowercase -> splits the string in a list
+    textlist = text.replace("\n", "").lower().split() #removes newline from string (\n) -> makes it all lowercase -> splits the string in a list
     #lower_case = [word.lower() for word in textlist]
     counts_text = collections.Counter(textlist)
     return counts_text
 
-def dataexport(data):
+def dataexport(data, filename):
     #exporting the data
     try:
-        writer = pd.ExcelWriter('export.xlsx', engine='xlsxwriter')
+        writer = pd.ExcelWriter(filename + 'count.xlsx', engine='xlsxwriter')
         df = pd.DataFrame((list(data.items())), columns=['words', 'count'])
         df.to_excel(writer, index=False)
         writer.save()
